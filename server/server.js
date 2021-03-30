8286a6868c42f4d413ee06431144636d3dfa3deb
Dali-Hili <you@example.com>
2021-03-30 12:22:46 +0200

    inserted all components

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./router');
-
+var cors = require("cors");
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
-
+app.use(cors());
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
 app.use('/bundles', router.bundles);
 // Handling AJAX requests to the API by passing off requests to the api router
